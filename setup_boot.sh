1f006033b21aab4b517bae48c50c0c0598fef771
deepak-isha <d.kumar-ext@sadhguru.org>
2025-07-28 14:14:24 +0530

    Updating setup_boot.sh to copy all files to single location

--- setup_boot.sh
+++ setup_boot.sh
@@ -7,6 +7,9 @@
 
 mkdir -p /home/pi/deepak/enhanced_dashboard_system/
 cp check_and_install_ensure_pip_and_python.env.py /home/pi/deepak/enhanced_dashboard_system/check_and_install_ensure_pip_and_python.env.py
+cp 99runinstallscript /home/pi/deepak/enhanced_dashboard_system/99runinstallscript
+cp setup_boot.sh /home/pi/deepak/enhanced_dashboard_system/setup_boot.sh
+
 sudo cp 99runinstallscript /etc/NetworkManager/dispatcher.d/99runinstallscript
 
 sudo chmod +x /etc/NetworkManager/dispatcher.d/99runinstallscript
