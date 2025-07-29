98f97ba35f48848d3464e64251acf3fddbcf4351
deepak-isha <d.kumar-ext@sadhguru.org>
2025-07-29 20:43:22 +0530

    rtc working and updated setup script for better approach with variables

--- setup_boot.sh
+++ setup_boot.sh
@@ -5,12 +5,20 @@
 #pi ALL=(ALL) NOPASSWD: /usr/bin/apt-get
 #then source this setup_boot.sh file to run the commands below.
 
-mkdir -p /home/pi/deepak/enhanced_dashboard_system/
-cp check_and_install_ensure_pip_and_python.env.py /home/pi/deepak/enhanced_dashboard_system/check_and_install_ensure_pip_and_python.env.py
-cp 99runinstallscript /home/pi/deepak/enhanced_dashboard_system/99runinstallscript
-cp setup_boot.sh /home/pi/deepak/enhanced_dashboard_system/setup_boot.sh
+# Configuration variables
+INSTALL_DIR="/home/pi/deepak/enhanced_dashboard_system/"
+NETWORK_DISPATCHER_DIR="/etc/NetworkManager/dispatcher.d/"
 
-sudo cp 99runinstallscript /etc/NetworkManager/dispatcher.d/99runinstallscript
+mkdir -p $INSTALL_DIR
+cp check_and_install_ensure_pip_and_python.env.py $INSTALL_DIR/check_and_install_ensure_pip_and_python.env.py
+cp 99runinstallscript $INSTALL_DIR/99runinstallscript
+cp setup_boot.sh $INSTALL_DIR/setup_boot.sh
+cp rtc_new.py $INSTALL_DIR/rtc_new.py
+cp robust_git_venv_setup.py $INSTALL_DIR/robust_git_venv_setup.py
 
-sudo chmod +x /etc/NetworkManager/dispatcher.d/99runinstallscript
-sudo chmod +x /home/pi/deepak/enhanced_dashboard_system/check_and_install_ensure_pip_and_python.env.py
+sudo cp 99runinstallscript $NETWORK_DISPATCHER_DIR/99runinstallscript
+
+sudo chmod +x $NETWORK_DISPATCHER_DIR/99runinstallscript
+sudo chmod +x $INSTALL_DIR/check_and_install_ensure_pip_and_python.env.py
+sudo chmod +x $INSTALL_DIR/rtc_new.py
+sudo chmod +x $INSTALL_DIR/robust_git_venv_setup.py
